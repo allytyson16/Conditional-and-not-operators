ff1a7be70a59a85eff713bbf97169dbe5bf922a3
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-01-30 23:48:18 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+//Conditional and not operators
+//(condition)?true:false
+				int x = 2;
+				int y = 8;
+				
+				int biggestNumber = x > y ? x : y;
+				Console.WriteLine(biggestNumber);
+		
+				int testScore = 3;
+				Console.WriteLine((testScore >= 70)? "Pass" : "Fail");
+		
+				//bool myBool = true;
+				//if(myBool)
+		
+				int myInt = 3;
+				if(!(myInt > 6))
+				{
+					Console.WriteLine("That is lower than expected");
+
+				}
+				else
+				{
+					Console.WriteLine("That is right");
+				}        }
     }
 }
